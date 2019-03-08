c3d2aab46498b466fefe2a6cf6af8f2bd8ddf6bf
Zhouyoulin <779606669@qq.com>
2019-03-08 19:29:04 +0800

    add 函数定义

--- day05/Method.py
+++ day05/Method.py
@@ -2,3 +2,27 @@
 def add(a, b):
     print("{0}+{1}={2}".format(a, b, a+b))
 add(5, 45)
+
+# 参数传值
+# 不可变对象传值
+def changeInt(a):
+    a = 20
+    print("函数内a的值为: ",a)
+b = 10
+changeInt(b)
+print(b)
+
+def changeStr(str):
+    str = "hello wrod"
+    print(str)
+str = "1a"
+changeStr(str)
+print(str)
+
+# 可变对象传值
+def changeList(list):
+    list.append([1, 2, 3, 4])
+    print(list)
+list = [12, 13, 14]
+changeList(list)
+print(list)
